d35617760c38f6c8659cbe6213e5220feb39c74a
Lundhede <m010795@hotmail.com>
2015-10-27 09:39:50 +0100

    Copyrigthish tilføjet + ændret filtype af lib

--- index.c
+++ index.c
@@ -1,7 +1,20 @@
+// This program has been devoloped at Aalborg Universitet by:
+// Alminde, Patrick Abildgaard --  palmin15@student.aau.dk
+// Hansen, Mathias Lundhede    --  mlha15@student.aau.dk
+// Jensen, Benjamin Rytter     --	 brje15@student.aau.dk
+// Juul-Nyholm, Markus Hald    --  mjuuln15@student.aau.dk
+// Kristiansen, Dan            --  dkris15@student.aau.dk
+// Ortmann, Kasper Heinrich    --  kortma15@student.aau.dk
+// Vive, Lasse Erritsø         --  lvive15@student.aau.dk
+
+
 #include <stdio.h>
+#include <stdlib.h>
+#include "p1lib.h"
 
 
 int main(void)
 {
-
+	
+	
 }
