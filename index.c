1f6e3f797db53f007a33ea11b35e153368c80e00
Lundhede <m010795@hotmail.com>
2015-10-27 12:22:13 +0100

    Stuff happened

--- index.c
+++ index.c
@@ -15,6 +15,40 @@
 
 int main(void)
 {
+	int ClassInput, classLoop, ClassYear, AmountOfStudents, classLoop2, Teacherloop, TeacherInput;
 	
+	while(classLoop != 1)
+	{
+		printf("Indtast en klasse\n");
+		scanf("%d", &ClassInput);
+		classLoop = checkClassInput(ClassInput);
+	}
+	
+	while(classLoop2 != 1)
+	{
+		printf("Indtast klassens årgang og antallet af elever i klassen");
+		scanf("%d%d", &ClassYear, &AmountOfStudents);
+		classLoop2 = checkYearSize(ClassYear, AmountOfStudents);
+	}
+	
+	while(TeacherLoop != 1)
+	{
+		printf("Indtast en lærers navn\n");
+		scanf("%", &TeacherInput);
+		classLoop = checkClassInput(TeacherInput);
+	}	
+	
+	
+	printf("");
+	scanf("");
+	checkTeacherSubject();
+	
+	printf("");
+	scanf("");
+	checkClassrooms();
+	
+	printf("");
+	scanf("");
+	checkClassExistence();
 	
 }
--- p1lib.h
+++ p1lib.h
@@ -1,1 +1,48 @@
- 
+int checkClassInput(int classChecker)
+{
+	if (classChecker)
+	{
+		return 1;
+	}
+	else 
+	{
+		return 0;
+	}
+}
+
+int checkYearAndSize(int ClassYear, int AmountOfStudents)
+{
+	if((ClassYear > 1900) && (AmountOfStudents >= 1))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int addTeacher(? TeacherInput)
+{
+	char NewTeacher;
+	while(NewTeacher != n)
+	{
+		printf("Tilføj en ny lære? (y/n)");
+		scanf("%c", &NewTeacher);
+	}
+}
+
+int checkTeacherSubject()
+{
+	
+}
+
+int checkClassrooms()
+{
+	
+}
+
+int checkClassExistence()
+{
+	
+}
